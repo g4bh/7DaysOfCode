f055b4be3b3c50ac59958992ea75f228a958280d
Gabu <44330034+g4bh@users.noreply.github.com>
2023-09-06 03:23:41 -0300

    DIA 1 - Arrays

---
+++ Desafio1_7daysOfCode/Desafio1_7daysOfCode/ListaDeCompras.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio1_7daysOfCode
+{
+    internal class ListaDeCompras
+    {
+        ArrayList itens = new ArrayList();
+        ArrayList quantidade = new ArrayList();
+
+
+        public void AdicionarItem(string nomeItem, int qtddItem)
+        {
+            itens.Add(nomeItem);
+            quantidade.Add(qtddItem);
+        }
+
+        public void RemoverItem(string nomeItem) {
+
+            for (int i = 0; i < itens.Count; i++)
+            {   
+                if (itens.IndexOf(nomeItem) == i)
+                {
+                    itens.RemoveAt(i);
+                    quantidade.RemoveAt(i);
+                }
+                else {
+                    Console.WriteLine("\nNão há nenhum item com esse valor!\n");
+                    Console.WriteLine(" ");
+                }
+                
+            }
+        }
+
+        public void ListarItens() {
+
+            if (itens.Count == 0)
+            {
+                Console.WriteLine("Não há itens na lista!\n");
+                Console.WriteLine(" ");
+            }
+            else
+            {
+                for (int i = 0; i < itens.Count; i++)
+                {
+                    Console.WriteLine($"\nItem: " + itens[i] + " | Quantidade: " + quantidade[i]);
+                    Console.WriteLine(" ");
+                }
+            }
+        }
+    }
+
+    
+}
---
+++ Desafio1_7daysOfCode/Desafio1_7daysOfCode/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Desafio1_7daysOfCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListaDeCompras listinha = new ListaDeCompras();
+            int opcao;
+
+            Console.WriteLine("\r\n▀▀ ▀▀ ▀▀ ▀▀ ▀▀   █░░ █ █▀ ▀█▀ ▄▀█   █▀▄ █▀▀   █▀▀ █▀█ █▀▄▀█ █▀█ █▀█ ▄▀█ █▀   ▀▀ ▀▀ ▀▀ ▀▀ ▀▀\r\n▀▀ ▀▀ ▀▀ ▀▀ ▀▀   █▄▄ █ ▄█ ░█░ █▀█   █▄▀ ██▄   █▄▄ █▄█ █░▀░█ █▀▀ █▀▄ █▀█ ▄█   ▀▀ ▀▀ ▀▀ ▀▀ ▀▀");
+            Console.WriteLine(" ");
+
+            do
+            {
+                Console.WriteLine("O que deseja fazer agora?\n\n1 - Adicionar um item na lista \n2 - Excluir um item na lista \n3- Mostrar a lista \n4 - Sair");
+                opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:
+                        Console.Write("Digite o nome do item: ");
+                        string nomeItem = Console.ReadLine();
+                        Console.Write("Adicione uma quantidade: ");
+                        int qttdItem = int.Parse(Console.ReadLine());
+                        listinha.AdicionarItem(nomeItem, qttdItem);
+                        Console.Write("\nItem adicionado com sucesso!\n");
+                        Console.WriteLine(" ");
+                        break;
+
+                    case 2:
+                        Console.Write("Digite o nome do produto que deseja excluir: ");
+                        nomeItem = Console.ReadLine();
+                        listinha.RemoverItem(nomeItem);
+                        Console.Write("\nItem excluído com sucesso!\n");
+                        break;
+
+                    case 3:
+                        listinha.ListarItens();
+                        break;
+                    
+                    case 4:
+                        break;
+                }
+            } while (opcao != 4);
+        }
+    }
+}
