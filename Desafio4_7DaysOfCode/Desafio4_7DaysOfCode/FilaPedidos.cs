985b3fd52929bc1d9e208ff6944e2abd2b7127b7
Gabu <44330034+g4bh@users.noreply.github.com>
2023-09-09 22:39:05 -0300

    DIA 4 - Filas

---
+++ Desafio4_7DaysOfCode/Desafio4_7DaysOfCode/FilaPedidos.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio4_7DaysOfCode
+{
+    internal class FilaPedidos
+    {
+        ArrayList nomePrato = new ArrayList();
+        ArrayList numeroMesa = new ArrayList();
+
+
+
+        public void CadastrarPedido(string nomeP, int numeroM) {
+            nomePrato.Add(nomeP);
+            numeroMesa.Add(numeroM);
+        }
+
+        public void DeletarPedido () {
+
+            nomePrato.RemoveAt(0);
+            numeroMesa.RemoveAt(0);
+
+        }
+
+        public void ListarPedido() {
+
+            if (nomePrato.Count == 0 || numeroMesa.Count == 0)
+            {
+                Console.WriteLine("Não há pedidos na lista!");
+            }
+            else {
+                Console.WriteLine("PEDIDOS: ");
+                for (int i = 0; i < nomePrato.Count; i++) {
+                    Console.WriteLine($"Nome do Prato:" + nomePrato[i] + " | Número da mesa: " + numeroMesa[i] );
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
---
+++ Desafio4_7DaysOfCode/Desafio4_7DaysOfCode/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Desafio4_7DaysOfCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var filaPedidos  = new FilaPedidos();
+
+            filaPedidos.CadastrarPedido("Bife a milanesa", 1);
+            filaPedidos.CadastrarPedido("Arroz com calabresa", 4);
+            filaPedidos.CadastrarPedido("Pizza", 2);
+            filaPedidos.CadastrarPedido("Batata assada", 3);
+            filaPedidos.CadastrarPedido("Camarão", 5);
+            filaPedidos.CadastrarPedido("Bolo", 6);
+
+            filaPedidos.ListarPedido();
+
+            filaPedidos.DeletarPedido();
+
+            filaPedidos.ListarPedido();
+
+            filaPedidos.DeletarPedido();
+
+            filaPedidos.ListarPedido();
+
+            filaPedidos.CadastrarPedido("Café", 3);
+
+            filaPedidos.ListarPedido();
+
+        }
+    }
+}
